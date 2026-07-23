8cdb703bc88307b671d9a3b7cf290ddeaedad5e4
Bruno <bruno.cavor@gmail.com>
2026-07-23 15:11:09 +0200

    Patch v1.3

--- luma-productions/components/VjencanjaHero.tsx
+++ luma-productions/components/VjencanjaHero.tsx
@@ -1,11 +1,8 @@
 import SectionHero from "./SectionHero";
 
 const heroImages = ["/heroVjencanja/desktop/Lucija%26Ante%20-%20188.jpg"];
-// Portrait crops for phones (heroVjencanja/mobile).
-const heroImagesMobile = [
-  "/heroVjencanja/mobile/Lucija%26Ante%20-%20182.jpg",
-  "/heroVjencanja/mobile/Lucija%26Ante%20-%20184.jpg",
-];
+// Portrait crop for phones (heroVjencanja/mobile).
+const heroImagesMobile = ["/heroVjencanja/mobile/Lucija%26Ante-184.jpg"];
 
 export default function VjencanjaHero() {
   return (
